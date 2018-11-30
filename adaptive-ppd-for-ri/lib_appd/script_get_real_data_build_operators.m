81a5e18c5c229713cff70e036c04dc06991ff2af
Arwa Dabbech <dabbecharoua@gmail.com>
2018-11-30 14:42:26 +0000

    minor change

--- adaptive-ppd-for-ri/lib_appd/script_get_real_data_build_operators.m
+++ adaptive-ppd-for-ri/lib_appd/script_get_real_data_build_operators.m
@@ -24,8 +24,8 @@
             if flag_single_data_set
                param_block.pos = length(uw);
             end
-            out_block =util_time_based_block_sp_ar(time,param_block);
-            partition = out_block.partition;
+            blockStruct =util_time_based_block_sp_ar(time,param_block);
+            partition = blockStruct.block;
         else
             partition = length(uw); % one block
         end
