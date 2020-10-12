4cb94adc501f5cb9fbd118e5e4670b06e6038b25
Arwa Dabbech <dabbecharoua@gmail.com>
2020-10-12 16:00:21 +0100

    added a flag for the weights saved in the data
    if flag_is_weightsFixed2sigma>0 then the weights are set to sigma, else weights correspond to 1/(sigma^2)

--- minimalist_script.m
+++ minimalist_script.m
@@ -35,10 +35,10 @@ param_global.Nx = 512; %image dim.
 param_global.Ny = 512; %image dim.
 
 param_global.pixelSize = 2.5; % 'in arcsec'
-
 param_global.ObsFreq = 4536E6;% freq in 'Hz'
 
 param_global.visibilityFileName = '3c391_I00' ;
+param_global.flag_is_weightsFixed2sigma = 0; % 'weights' in the data file corresponds to SIGMA.
 
 %solver
 param_global.l1_reg = 1e-4;% soft-thresholding param. of SARA
